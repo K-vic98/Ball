1d5bd41a09e7749d37120d2ff059ac1d00358051
Victor Kudelin <58315363+K-vic98@users.noreply.github.com>
2021-03-09 17:16:51 +0300

    Made display of score on the screen

--- Assets/Scripts/Ball/Ball.cs
+++ Assets/Scripts/Ball/Ball.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Ball : MonoBehaviour
 {
-    
+    private int _score;
+
+    public event UnityAction<int> ScoreChanged;
+    public event UnityAction PlayerDied;
+
+    public void IncreaseScore()
+    {
+        _score++;
+        ScoreChanged?.Invoke(_score);
+    }
+
+    public void Die()
+    {
+
+    }
 }
---
+++ Assets/Scripts/Ball/BallCollisionHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Ball))]
+public class BallCollisionHandler : MonoBehaviour
+{
+    private Ball _ball;
+
+    private void Awake()
+    {
+        _ball = GetComponent<Ball>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Coin>(out Coin coin))
+        {
+            _ball.IncreaseScore();
+        }
+
+        if (other.TryGetComponent<Barrier>(out Barrier barrier))
+        {
+            _ball.Die();
+        }
+    }
+}
